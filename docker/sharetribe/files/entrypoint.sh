c348e1fe22d7eead9b47baf4b7aa08e8830d6146
Venky <45107241+business-hall@users.noreply.github.com>
2019-01-12 15:21:13 -0600

    Add code to copy config

--- docker/sharetribe/files/entrypoint.sh
+++ docker/sharetribe/files/entrypoint.sh
@@ -1,2 +1,17 @@
 source /etc/profile.d/rvm.sh
+srcBaseDir=/extvol/sharetribe/config
+dstBaseDir=/opt/app/config
+for f in config.defaults config gmaps_api_key secrets;
+do
+  src=${srcBaseDir}/${f}.yml
+  if test -f ${src}; then
+    dst=${dstBaseDir}/${f}.yml
+    if test -f ${dst}; then
+      echo "Removing ${dst}"
+      rm ${dst}
+    fi
+    echo "Copying ${src} to ${dst}"
+    cp ${src} ${dst}
+  fi
+done
 /usr/bin/supervisord --configuration /sharetribe/etc/supervisord.conf
