cf7b750a0f7763f259eb42523030d343ef093558
Venky <45107241+business-hall@users.noreply.github.com>
2019-01-13 10:04:15 -0600

    Make deployment easier

--- docker/sharetribe/files/entrypoint.sh
+++ docker/sharetribe/files/entrypoint.sh
@@ -1,17 +1,36 @@
 source /etc/profile.d/rvm.sh
-srcBaseDir=/extvol/sharetribe/config
-dstBaseDir=/opt/app/config
-for f in config.defaults config gmaps_api_key secrets;
+cfgSrcDir=/extvol/sharetribe/config
+cfgDstDir=/opt/app/config
+for f in `ls ${cfgSrcDir}`;
 do
-  src=${srcBaseDir}/${f}.yml
-  if test -f ${src}; then
-    dst=${dstBaseDir}/${f}.yml
-    if test -f ${dst}; then
-      echo "Removing ${dst}"
-      rm ${dst}
-    fi
-    echo "Copying ${src} to ${dst}"
-    cp ${src} ${dst}
+  dst=${cfgDstDir}/${f}
+  if test -f ${dst}; then
+    echo "Removing ${dst}"
+    rm ${dst}
   fi
+  src=${cfgSrcDir}/${f}  
+  echo "Copying ${src} to ${dst}"
+  cp ${src} ${dst}
 done
+nginx -t
+if test $? -ne 0; then
+  echo "ERROR: NGINX config check failed. Double check !"
+  exit 1
+fi
+extPubDir=/extvol/sharetribe/public
+intPubDir=/opt/app/public
+builtPubDir=/opt/app/private
+if test ! -d ${extPubDir}/system/paypal; then
+  echo "Public dir ${extPubDir} uninitialized. Fixing .."
+  if test -L ${intPubDir}; then
+    rm ${intPubDir}
+  fi
+  if test -d ${intPubDir}; then
+    rm -rf ${intPubDir}
+  fi
+  mkdir -p ${extPubDir}/system/paypal
+  cp -R ${builtPubDir}/* ${extPubDir}/
+  chmod -R 777 ${extPubDir}
+  ln -s ${extPubDir} ${intPubDir}
+fi  
 /usr/bin/supervisord --configuration /sharetribe/etc/supervisord.conf
